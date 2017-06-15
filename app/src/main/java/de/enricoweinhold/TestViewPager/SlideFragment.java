5c6811b434d54db8530f877c43c2d112f9239bab
Enrico Weinhold <mail@enricoweinhold.de>
2017-06-15 19:00:57 +0200

    initial commit

---
+++ app/src/main/java/de/enricoweinhold/TestViewPager/SlideFragment.java
@@ -0,0 +1,27 @@
+package de.enricoweinhold.TestViewPager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class SlideFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        //Get the ViewGroup that contains your layout
+        ViewGroup viewGroup = (ViewGroup)inflater.inflate(R.layout.viewpager_content, container, false);
+        TextView textView = (TextView) viewGroup.findViewById(R.id.textview);
+
+        //Get pagenumber from arguments
+        Bundle bundle = this.getArguments();
+        textView.setText(bundle.getInt("PAGENUMBER") + "");
+
+        return viewGroup;
+    }
+}
